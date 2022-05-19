86c98d4c836663468d4b39c553c9df3ed01ace19
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-05-19 14:18:04 +0530

    difference

--- day3Assignment/StaticNonStatic.java
+++ day3Assignment/StaticNonStatic.java
@@ -2,9 +2,24 @@ package com.day3Assignment;
 
 public class StaticNonStatic {
 
-// Static Variables:
+// Static Variables: static Variables which we can access directly inside the main method by using three ways
+//are as follows
+		
+//	Non Static Variables: non-static Variables are not directly accessible inside the static area i.e those elements which we can not directly access in the main method,
+//we should have to create an object to access that variable 
 	
+	int age = 23;//non_static Variable
+	static int marks=90;//static variable
 	
+	public static void main(String[] args) {
+		System.out.println(marks);//1.directly access
+		//or
+		System.out.println(StaticNonStatic.marks);//2.using class name dot
+		//or
+		StaticNonStatic st = new StaticNonStatic();
+		System.out.println(st.marks);//3.using object 
+		
+		System.out.println(st.age);//access using object only
+	}
 	
-//	Non Static Variables:
 }
