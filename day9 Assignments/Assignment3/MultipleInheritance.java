83bd55261a5f101b52e3ea70ccb33385ca988346
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-05-31 14:22:38 +0530

    day9 Assignments

---
+++ day9 Assignments/Assignment3/MultipleInheritance.java
@@ -0,0 +1,17 @@
+package com.Assignment3;
+
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Why is  multiple Inheritance is not supported in java at class level?
+		
+//		for prevent ambiguity
+//		consider in case if class C extends class A and class B 
+//		if class A and class B both have same method run()
+//		then java compiler cannot decide, which run () method should inherit.
+//		To prevent such situation, multiple inheritance not supported in java
+	}
+
+}
