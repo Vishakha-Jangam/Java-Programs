ea016cf4f9fa86b846fec297bac447cee43a7465
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-06-01 22:54:05 +0530

    last Assignment

--- day5 Assignments/Assignment4/Demo.java
+++ day5 Assignments/Assignment4/Demo.java
@@ -1,8 +1,47 @@
 package com.Assignment4;
 
+import java.util.Scanner;
+
 public class Demo {
 	public static void main(String[] args) {
 		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("Please enter Number of Students:");
+		int num=sc.nextInt();
+		
+		StudentBean [] students =new StudentBean[num];
+		
+		for(int i=0;i<students.length;i++) {
+			System.out.println("Enter the details of Student"+(i+1));
+			System.out.println("Enter Roll no:");
+			int roll =sc.nextInt();
+			
+			System.out.println("Enter Name");
+			String name=sc.next();
+			
+			System.out.println("Enter Marks:");
+			int marks=sc.nextInt();
+			
+			System.out.println("Enter Address:");
+			String address=sc.next();
+			
+			students[i]=new StudentBean(roll,name,address,marks);
+		}
+		
+		
+		System.out.println("Student Details are:");
+		System.out.println("====================");
+		int sum=0;
+		for(StudentBean student:students) {
+			System.out.println("Roll no is:"+student.getRoll());
+			System.out.println("Name is:"+student.getName());
+			System.out.println("Marks is:"+student.getMarks());
+			sum+=student.getMarks();
+			System.out.println("##########################");
+		}
+		
+		System.out.println("Average of marks:"+sum/students.length);
 		
 	}
 }
--- day5 Assignments/Assignment4/StudentBean.java
+++ day5 Assignments/Assignment4/StudentBean.java
@@ -6,4 +6,40 @@ public class StudentBean {
 	String name;
 	String address;
 	int marks;
+	public StudentBean(int roll2, String name2,String address, int marks2) {
+		this.roll=roll2;
+		this.name=name2;
+		this.address=address;
+		this.marks=marks2;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	
+
+		
+		
+	
 }
